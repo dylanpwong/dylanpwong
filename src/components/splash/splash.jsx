6e58f8be72f338e9c9a6684b3e07e23cd0267198
dpwong <dylanpwong@gmail.com>
2020-09-21 21:11:21 -0400

    bg updated

--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import './splash.css'
-// import magicCss from "magic.css";
+import './splash.scss'
+import cafe from "../../images/cafe.gif";
+import backStreets from '../../images/backstreets.gif'
 class Splash extends React.Component{
     // constructor(props){
     //     super(props);
     // }
 
     render(){
-        return(
-            <div className='magictime puffIn' >
-                Dylan
+        // let m = magicCss
+        return (
+          <div className="mainContentContainer">
+              <div className ='NavBar'> 
+                  Dylan Wong
+              </div>
+              <div className='WelcomeButton'>
+                  Welcome
+              </div>
+            <div className='backgroundImgContainer'> {/*pos abs ignores flex*/}
+              <img className='backgroundImg' src={backStreets} alt="cafe gif!" />
             </div>
-        )
+          </div>
+        );
     }
 }
 

0a08ffea7748e1e02c902ee5f064aee94bd73cf0
dpwong <dylanpwong@gmail.com>
2020-09-23 22:01:59 -0400

    animation effects added

--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './splash.scss'
 // import cafe from "../../images/cafe.gif";
 import backStreets from '../../images/backstreets.gif'
+import profilePic from '../../images/profile1.jpeg'
 class Splash extends React.Component{
     // constructor(props){
     //     super(props);
@@ -14,8 +15,15 @@ class Splash extends React.Component{
               <div className ='NavBar'> 
                   Dylan Wong
               </div>
-              <div className='WelcomeButton'>
-                  Welcome
+              <div className='profilePictureContainer'>
+                <img className='profilePic' src={profilePic} alt=""/>
+              </div>
+              <div className='typeWritterContainer'> {/* type writter container */}
+                <div className='typewritter'>
+                    <h1>
+                        Welcome
+                    </h1>
+                </div>
               </div>
             <div className='backgroundImgContainer'> {/*pos abs ignores flex*/}
               <img className='backgroundImg' src={backStreets} alt="cafe gif!" />

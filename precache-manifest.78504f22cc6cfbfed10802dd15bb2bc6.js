b8425603549fbc2e096d99c60beed5e7b63350e7
dpwong <dylanpwong@gmail.com>
2020-10-03 13:53:48 -0400

    Updates

---
+++ precache-manifest.78504f22cc6cfbfed10802dd15bb2bc6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58722cabd05c15c7deba36f5c612dd3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbfb747dcc1e04500fa5",
+    "url": "/static/css/main.ebfe2ae3.chunk.css"
+  },
+  {
+    "revision": "3c78b176258c3b7f60ae",
+    "url": "/static/js/2.db1ea1ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db1ea1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfb747dcc1e04500fa5",
+    "url": "/static/js/main.9535523a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "9145f1dd06bd395ba2686489d82a3e46",
+    "url": "/static/media/GROBOLD.9145f1dd.ttf"
+  },
+  {
+    "revision": "ae3799c1114f374948b46398069cf449",
+    "url": "/static/media/backstreets.ae3799c1.gif"
+  },
+  {
+    "revision": "e058fa970fc282e03edc68ff4feb32d9",
+    "url": "/static/media/cafe.e058fa97.gif"
+  },
+  {
+    "revision": "6be89b96350815f624315e0096b72cbc",
+    "url": "/static/media/cherry3.6be89b96.gif"
+  },
+  {
+    "revision": "8c7880d87d40918ec9c70e9de12a219d",
+    "url": "/static/media/cnr.8c7880d8.otf"
+  },
+  {
+    "revision": "3c2a34886f0956c9141b8d96423a3b9e",
+    "url": "/static/media/deceptionGamePage.3c2a3488.png"
+  },
+  {
+    "revision": "d14d1521b7b5b046f669f0cdb5695091",
+    "url": "/static/media/gameplay.d14d1521.png"
+  },
+  {
+    "revision": "484b325cdb5bdd8bce6d8e3d131fda75",
+    "url": "/static/media/openfree.484b325c.gif"
+  },
+  {
+    "revision": "08eba1de6100c1f367c847c512ba0ad4",
+    "url": "/static/media/ramenShop.08eba1de.gif"
+  },
+  {
+    "revision": "b83762315b8661708b883f2faf1c4203",
+    "url": "/static/media/resizedRC.b8376231.png"
+  },
+  {
+    "revision": "b9fe382f4544dffedb6237b83522d3cf",
+    "url": "/static/media/resume.b9fe382f.pdf"
+  },
+  {
+    "revision": "6f847ebadb38ea131f8351689dc20c0d",
+    "url": "/static/media/sideshot.6f847eba.jpg"
+  },
+  {
+    "revision": "d1588b423d9140f6a88e2f18941c25af",
+    "url": "/static/media/steamss.d1588b42.png"
+  }
+]);
